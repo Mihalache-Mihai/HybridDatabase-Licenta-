cf58fa096193bbfef53df4e075c6a246ebb2f4f1
Mihalache Mihai <mihalachemihai30@gmail.com>
2019-03-23 19:21:15 +0200

    Added hstore as attribute

--- src/main/java/com/licenta/models/Task.java
+++ src/main/java/com/licenta/models/Task.java
@@ -1,25 +1,38 @@
 package com.licenta.models;
 
+import com.vladmihalcea.hibernate.type.basic.PostgreSQLHStoreType;
+import lombok.Data;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
-@Entity
-public class Task extends BaseEntity<Long> implements Serializable{
+@Data
+@Entity(name = "Task")
+@Table(name ="task")
+@TypeDef(name="hstore",typeClass = PostgreSQLHStoreType.class)
+public class Task implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(unique = true, nullable = false)
+    private Long ID;
+
 
     @Column(name="description")
     private String description;
 
+
+    @Type(type="hstore")
+    @Column(columnDefinition = "hstore")
+    private Map<String,String> properties = new HashMap<>();
+
     protected Task() { }
 
     public Task(String description) {
-        this.description = description;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
         this.description = description;
     }
 }
--- src/test/java/com/licenta/HybridDatabaseTests.java
+++ src/test/java/com/licenta/HybridDatabaseTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringbootAuthUpdatedApplicationTests {
+public class HybridDatabaseTests {
 
 	@Test
 	public void contextLoads() {
